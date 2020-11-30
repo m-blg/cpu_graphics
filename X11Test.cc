a0db66f7156d6cecc6158dd28776a3ab089a26dc
mbgl <caaalm3@gmail.com>
2020-12-01 01:57:49 +0300

    X11 template

--- X11Test.cc
+++ X11Test.cc
@@ -40,15 +40,17 @@ int main() {
     Colormap color_map = XCreateColormap(display, root, visual_info.visual, AllocNone);
 
     XSetWindowAttributes attributes;
+    attributes.bit_gravity = StaticGravity;
     attributes.background_pixel = 0;
     attributes.colormap = color_map;
-    attributes.event_mask = SubstructureNotifyMask | KeyPressMask | KeyReleaseMask;
-    u64 attribute_mask = CWBackPixel | CWColormap | CWEventMask;
+    attributes.event_mask = StructureNotifyMask | KeyPressMask | KeyReleaseMask;
+    u64 attribute_mask = CWBitGravity | CWBackPixel | CWColormap | CWEventMask;
 
+    vec2i window_max_size = {1920, 1080};
     vec2i window_size = {640, 480};
     Window window = XCreateWindow(display, root, 100, 100, window_size.x, window_size.y, 0, visual_info.depth, InputOutput, visual_info.visual, attribute_mask, &attributes);
 
-    x_set_size_hint(display, &window, {100, 100}, {1280, 720});
+    x_set_size_hint(display, &window, {100, 100}, window_max_size);
 
     XStoreName(display, window, "X11 Test");
     XMapWindow(display, window);
@@ -58,16 +60,16 @@ int main() {
     // create buffer
 
     dbuff2<u32> window_buffer;
-    window_buffer.init(window_size.y, window_size.x);
-    XImage* x_window_buffer = XCreateImage(display, visual_info.visual, visual_info.depth, ZPixmap, 0, (char*)window_buffer.buffer, window_size.x, window_size.y, sizeof(u32) * 8, 0);
+    window_buffer.init(window_max_size.y, window_max_size.x);
+    XImage* x_window_buffer = XCreateImage(display, visual_info.visual, visual_info.depth, ZPixmap, 0, (char*)window_buffer.buffer, window_max_size.x, window_max_size.y, sizeof(u32) * 8, 0);
 
 
-    // event loop
     bool is_running = true;
     while (is_running) 
     {
-        XEvent e;
+        // event loop
 
+        XEvent e;
         while (XPending(display) > 0) {
             XNextEvent(display, &e);
             switch (e.type) {
@@ -77,19 +79,24 @@ int main() {
                         is_running = false;
                     }
                 } break;
+                case ConfigureNotify: {
+                    XConfigureEvent* configure_event = (XConfigureEvent*)&e;
+                    window_size.x = configure_event->width;
+                    window_size.y = configure_event->height;
+                } break;
                 case KeyPress: {
                     XKeyEvent* key_event = (XKeyEvent*)&e;
                     if (key_event->keycode == XKeysymToKeycode(display, XK_q)) {
                         is_running = false;
                     }
-                }
+                } break;
             }
         }
 
         // write to buffer
 
         for (u32 i = 0u; i < window_size.y; i++) {
-            for (u32 j = 0u; j < window_size.y; j++) {
+            for (u32 j = 0u; j < window_size.x; j++) {
                 if (((i/100u) + (j/100u)) % 2 > 0) {
                     window_buffer.get(i, j) = 0xffffffff;
                 } else {
